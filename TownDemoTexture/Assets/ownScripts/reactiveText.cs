41d2810212ac7bb1d343ed8737a627ef2b8b094f
X-Hades-X <maurus.daehler@gmail.com>
2014-10-29 04:22:51 +0100

    Implemented StartMenu

---
+++ TownDemoTexture/Assets/ownScripts/reactiveText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reactiveText : MonoBehaviour {
+
+	public bool isQuit = false, isStart= false;
+	void OnMouseEnter(){
+		GetComponent<TextMesh> ().color = Color.red;
+		}
+
+	void OnMouseExit(){
+		GetComponent<TextMesh> ().color = Color.black;
+		}
+
+	void OnMouseDown(){
+				if (isQuit) {
+						Application.Quit ();
+				} else if (isStart) {
+						Application.LoadLevel (2);
+				} else {
+						Application.LoadLevel (1);
+				}
+		}
+
+}
