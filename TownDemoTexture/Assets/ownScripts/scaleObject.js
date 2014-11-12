7beb3afd29fba71b2de3e7b30182c5a8277945b9
X-Hades-X <maurus.daehler@gmail.com>
2014-11-12 01:45:58 +0100

    Latest Working Build
    Fehler auf Tower, letzter funktionierender Build vom Laptop hochladen

--- TownDemoTexture/Assets/ownScripts/scaleObject.js
+++ TownDemoTexture/Assets/ownScripts/scaleObject.js
@@ -5,14 +5,14 @@ function Start () {
 }
 
 function Update () {
-	if(Input.GetKey(KeyCode.UpArrow)){
+	if(Input.GetButton("RiseHigh")){
 		for(var child : Transform in transform){
 			child.position += Vector3(0,-0.1,0);
 			child.localScale += Vector3(0,0.01,0);
 		}
 	}
 	
-	if(Input.GetKey(KeyCode.DownArrow)){
+	if(Input.GetButton("RiseDeep")){
 		for(var child : Transform in transform){
 			child.position += Vector3(0,0.1,0);
 			child.localScale += Vector3(0,-0.01,0);
