12643c2c44c95e86935e19ef3ccc8d312f3bb761
X-Hades-X <maurus.daehler@gmail.com>
2014-11-14 08:30:56 +0100

    new gitignore

--- TownDemoTexture/Assets/ownScripts/scaleObject.js
+++ TownDemoTexture/Assets/ownScripts/scaleObject.js
@@ -1,20 +1,18 @@
 ﻿#pragma strict
 
-function Start () {
-
-}
+var transformNotCentered:boolean;
 
 function Update () {
 	if(Input.GetButton("RiseHigh")){
 		for(var child : Transform in transform){
-			child.position += Vector3(0,-0.1,0);
+			if(transformNotCentered)child.position += Vector3(0,-0.1,0);
 			child.localScale += Vector3(0,0.01,0);
 		}
 	}
 	
 	if(Input.GetButton("RiseDeep")){
 		for(var child : Transform in transform){
-			child.position += Vector3(0,0.1,0);
+			if(transformNotCentered)child.position += Vector3(0,0.1,0);
 			child.localScale += Vector3(0,-0.01,0);
 		}
 	}
